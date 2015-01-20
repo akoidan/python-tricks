69e913d3ebb632b9da410192839c1f18354c3502
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2015-01-20 16:28:45 +0200

    added ivyto gradle

---
+++ ivy-to-gradle.py
@@ -0,0 +1,27 @@
+__author__ = 'andrew'
+import re
+import sys
+
+def get_conf(conf):
+    return {
+        'default': 'runtime',
+        'runtime': 'runtime',
+        'test': 'testCompile',
+    }[conf]
+
+
+if __name__ == "__main__":
+    p = re.compile(r'<dependency(?=.*org="([.\-a-z0-9]+)")(?=.*name="([.\-a-z0-9]+)")(?=.*rev="([.\-a-z0-9]+))(?=.*conf="([.a-z0-9]+).*)')
+    files_count = len(sys.argv)-1
+    if files_count == 0:
+        print("Pass files as parameters example `python3 converter.py ../lol/ivy.xml`")
+    else:
+        for file in range(1, files_count+1):
+            with open(sys.argv[file], 'r') as f:
+                print('dependencies {\n ')
+                content = f.readlines()
+                for line in content:
+                    a = re.search(p, line)
+                    if a is not None:
+                        print("    %s \"%s:%s:%s\"" % (get_conf(a.group(4)), a.group(1), a.group(2), a.group(3)))
+                print('\n}')
