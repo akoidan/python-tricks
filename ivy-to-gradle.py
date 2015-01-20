b815e2436e298990f967a5dcf7f67ae41f003124
Andrey Koidan <ext_pnn_andrey.koidan@life.com.ua>
2015-01-20 17:39:01 +0200

    fixed unknown conf error, add verbose files

--- ivy-to-gradle.py
+++ ivy-to-gradle.py
@@ -3,11 +3,16 @@ import re
 import sys
 
 def get_conf(conf):
-    return {
-        'default': 'runtime',
-        'runtime': 'runtime',
-        'test': 'testCompile',
-    }[conf]
+    try:
+        return {
+            'default': 'compile',
+            'runtime': 'compile',
+            'test': 'testCompile',
+            'compile': 'compile',
+        }[conf]
+    except KeyError as e:
+        return None
+
 
 
 if __name__ == "__main__":
@@ -18,10 +23,14 @@ if __name__ == "__main__":
     else:
         for file in range(1, files_count+1):
             with open(sys.argv[file], 'r') as f:
-                print('dependencies {\n ')
+                print('Parsing file %s\n dependencies {\n ' % f.name)
                 content = f.readlines()
                 for line in content:
                     a = re.search(p, line)
                     if a is not None:
-                        print("    %s \"%s:%s:%s\"" % (get_conf(a.group(4)), a.group(1), a.group(2), a.group(3)))
+                        if get_conf(a.group(4)) is not None:
+                            print("    %s \"%s:%s:%s\"" % (get_conf(a.group(4)), a.group(1), a.group(2), a.group(3)))
+                        else:
+                            print("    %s \"%s:%s:%s\"" % ('compile', a.group(1), a.group(2), a.group(3)))
+                            print("can't parse line because of unknown conf" + line)
                 print('\n}')
