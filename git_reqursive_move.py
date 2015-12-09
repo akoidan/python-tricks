6405633063801865cf82b7688d21a2bea07bf32b
Andrew <nightmare.quake@Mail.ru>
2015-12-09 18:04:23 +0200

    Create git_reqursive_move.py

---
+++ git_reqursive_move.py
@@ -0,0 +1,13 @@
+dir = "src"
+to_dir = "dst"
+
+with open("%s/lol" % dir) as f:
+	content = f.readlines()
+	actions = {}
+	for line in content:
+		line = line.strip()
+		rsplit = line.rsplit('/', 1)
+		class_name = rsplit[-1].strip()
+		path = rsplit[0].strip()
+		print("mkdir -p %s/%s " % (to_dir, path))
+		print("git mv %s/%s %s/%s" % (dir, line, to_dir, path))
